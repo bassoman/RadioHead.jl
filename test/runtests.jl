e370a69d1e88131e4576010f77dafab9fc299f62
Jon Lancelle <bass@knosys.com>
2022-02-22 01:53:18 +0000

    conversion factor func added

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,18 @@ using RadioHead
 
 c = Coax(15.0, 0.8)
 
+@testset "units" begin
+    
+    m = Meter(2.0)
+    @test toFeet(m).value == 3.2808f0 * 2
+    @test toMeters(m).value == 1.0f0 * 2
+
+    m = Foot(3.0)
+    @test toMeters(m).value == 0.3048f0 * 3
+    @test toFeet(m).value == 1.0f0 * 3
+
+end
+
 @testset "coax" begin
 
     @test c.centerFreq === 15.0f0
